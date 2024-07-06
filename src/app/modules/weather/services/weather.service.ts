2b94a27e3f79fed6e55e961cbd9c6200e25c4677
Dante Menezes <dgdmenezes@gmail.com>
2024-07-05 22:22:52 -0300

    Adicionado modelo de interface

--- src/app/modules/weather/services/weather.service.ts
+++ src/app/modules/weather/services/weather.service.ts
@@ -1,9 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import {Observable} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class WeatherService {
+  private apiKey = "fba51f9789e4be7dc6a6e89b3975c2fb"
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getWeatherDatas(cityName: string): Observable<any> {
+    return this.http.get(
+      `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&mode=json&appid=${this.apiKey}`,
+    {}
+    );
+  }
 }
